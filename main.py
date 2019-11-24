6ec5ea9d47940a9a00e5778f2156d2dba9541f8a
adnanzaih <58091710+adnanzaih@users.noreply.github.com>
2019-11-24 09:31:15 -0700

    shortlisting cv2.findContours based on area size

--- main.py
+++ main.py
@@ -1,43 +1,41 @@
-import argparse
-import pydicom
-from skimage.morphology import square, disk
-from skimage.morphology import white_tophat
-from skimage.filters.rank import bottomhat, tophat
-import cv2
-from tqdm import tqdm
+# Python code to detect an arrow (seven-sided shape) from an image.
 import numpy as np
-import time
+import cv2
 
+# Reading image
+img2 = cv2.imread('shapes.png', cv2.IMREAD_COLOR)
 
-def Analyze(input):
-    path = "/Users/adnanhafeez/Documents/Python Programming/PRACTICE1/wintson lutz results/Z20190312/RI.Z20190312.C0 G0 T0-2_1_19.dcm"
-    filename = pydicom.read_file(input)
-    img = filename.pixel_array
-    detectField(img)
+# Reading same image in another variable and
+# converting to gray scale.
+img = cv2.imread('shapes.png', cv2.IMREAD_GRAYSCALE)
 
+# Converting image to a binary image
+# (black and white only image).
+_,threshold = cv2.threshold(img, 110, 255,
+							cv2.THRESH_BINARY)
 
-def detectField(input):
+# Detecting shapes in image by selecting region
+# with same colors or intensity.
+contours,_=cv2.findContours(threshold, cv2.RETR_TREE,
+							cv2.CHAIN_APPROX_SIMPLE)
 
-    input = input.astype('uint8')
-    input = tophat(input, disk(1))
-    smoothing = gaussian_kernel(size=10)
-    edge = cv2.filter2D(input, -1, smoothing)
-    cv2.imshow('Field', edge)
-    cv2.waitKey(0)
-    return
+# Searching through every region selected to
+# find the required polygon.
+for cnt in contours :
+	area = cv2.contourArea(cnt)
 
+	# Shortlisting the regions based on there area.
+	if area > 400:
+		approx = cv2.approxPolyDP(cnt,
+								0.009 * cv2.arcLength(cnt, True), True)
 
-def gaussian_kernel(size, sigma=5):
-    size = int(size) // 2
-    x, y = np.mgrid[-size:size+1, -size:size+1]
-    normal = 1 / (2.0 * np.pi * sigma**2)
-    g =  np.exp(-((x**2 + y**2) / (2.0*sigma**2))) * normal
-    return g
+		# Checking if the no. of sides of the selected region is 7.
+		if(len(approx) == 7):
+			cv2.drawContours(img2, [approx], 0, (0, 0, 255), 5)
 
+# Showing the image along with outlined arrow.
+cv2.imshow('image2', img2)
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-input', dest='input', help='Winston-Lutz dicom path', type=str)
-results = parser.parse_args()
-Analyze(results.input)
-
-
+# Exiting the window if 'q' is pressed on the keyboard.
+if cv2.waitKey(0) & 0xFF == ord('q'):
+	cv2.destroyAllWindows()
