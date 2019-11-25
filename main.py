0ee757f9249bd48ddb1bc07faf089ec6daa92d78
adnanzaih <58091710+adnanzaih@users.noreply.github.com>
2019-11-24 23:49:36 -0700

    shortlist contour using contourArea; need to check code against WL images

--- main.py
+++ main.py
@@ -1,41 +1,48 @@
-# Python code to detect an arrow (seven-sided shape) from an image.
-import numpy as np
+import argparse
+import pydicom
+from skimage.morphology import square
+from skimage.morphology import white_tophat
+from skimage.filters.rank import bottomhat
 import cv2
+from tqdm import tqdm
+import numpy as np
+import time
 
-# Reading image
-img2 = cv2.imread('shapes.png', cv2.IMREAD_COLOR)
 
-# Reading same image in another variable and
-# converting to gray scale.
-img = cv2.imread('shapes.png', cv2.IMREAD_GRAYSCALE)
+def Analyze(input):
+    filename = pydicom.read_file(input)
+    detectField(filename.pixel_array)
 
-# Converting image to a binary image
-# (black and white only image).
-_,threshold = cv2.threshold(img, 110, 255,
-							cv2.THRESH_BINARY)
 
-# Detecting shapes in image by selecting region
-# with same colors or intensity.
-contours,_=cv2.findContours(threshold, cv2.RETR_TREE,
-							cv2.CHAIN_APPROX_SIMPLE)
+def detectField(input):
+    #radiation field detection and contouring
+    kernal = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
+    output = bottomhat(input, kernal)  # disk(30))
+    blur = cv2.GaussianBlur(output, (5, 5), 0)
+    _, threshold = cv2.threshold(blur, 230, 255, 0)  # field
+    threshold = threshold.astype('uint8')
+    contours, hierarchy = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    for cnt in contours:
+        for i in tqdm(np.arange(len(contours))):
+        # Shortlisting the regions based on there area.
+            if cv2.contourArea(cnt) > 2000:
+                print(cv2.contourArea(cnt))
+                approx = cv2.approxPolyDP(cnt, 0.009 * cv2.arcLength(cnt, True), True)
+                # Checking if the no. of sides of the selected region is 4.
+                cv2.drawContours(threshold, [approx], -1, (78, 55, -128), 1)
+                M = cv2.moments(cnt)
+                time.sleep(0.1)
+        i = i + 1
+    cX = int(M["m10"] / M["m00"])
+    cY = int(M["m01"] / M["m00"])
+    print (cX, cY)
+    cv2.circle(threshold, (cX, cY), 2, (78, 55, -128), 1)
+    cv2.putText(threshold, "center", (cX - 20, cY - 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
+    cv2.imshow("Image", threshold)
+    cv2.waitKey(0)
 
-# Searching through every region selected to
-# find the required polygon.
-for cnt in contours :
-	area = cv2.contourArea(cnt)
 
-	# Shortlisting the regions based on there area.
-	if area > 400:
-		approx = cv2.approxPolyDP(cnt,
-								0.009 * cv2.arcLength(cnt, True), True)
-
-		# Checking if the no. of sides of the selected region is 7.
-		if(len(approx) == 7):
-			cv2.drawContours(img2, [approx], 0, (0, 0, 255), 5)
-
-# Showing the image along with outlined arrow.
-cv2.imshow('image2', img2)
-
-# Exiting the window if 'q' is pressed on the keyboard.
-if cv2.waitKey(0) & 0xFF == ord('q'):
-	cv2.destroyAllWindows()
+parser = argparse.ArgumentParser()
+parser.add_argument('-input', dest='input', help='sum the integers (default: find the max)', type=str)
+results = parser.parse_args()
+Analyze(results.input)
